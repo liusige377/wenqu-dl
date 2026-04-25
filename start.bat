2feab9f9d0815d964acdf20d04a992be57c1b6a1
liusige377 <630941605@qq.com>
2026-04-26 06:54:03 +0800

    fix: start.bat use full node path to fix startup error

--- start.bat
+++ start.bat
@@ -2,4 +2,5 @@
 :: 问渠下载器 v3.0 - 后台启动脚本
 :: 被任务计划程序调用，或手动运行
 cd /d "%~dp0"
-start "" /MIN node server.js
+:: 使用完整路径，避免cmd找不到node
+"C:\Program Files\QClaw\resources\openclaw\config\bin\node.cmd" server.js
